72bb7a1af90c1b33414e62bd254ba9faf877d64a
soulaymanebe <soulaymanebelhaj94@gmail.com>
2024-11-06 03:57:54 +0100

    Add more features (Seasons and episodes list, 404 page and caching)

--- app.py
+++ app.py
@@ -1,48 +1,95 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
+from flask_caching import Cache
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 OMDB_API_KEY = 'OMDB-API-Token'
 
-@app.route('/', methods=['GET', 'POST'])
+# Main endpoint
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        movie_name = request.form.get('movieName')
-        search_url = f'http://www.omdbapi.com/?apikey={OMDB_API_KEY}&s={movie_name}'
-        search_response = requests.get(search_url).json()
+    return render_template('index.html')
 
-        if search_response.get('Response') == 'True':
-            results = search_response.get('Search', [])
+# Results endpoint with caching
+@cache.cached(timeout=300, query_string=True)
+@app.route('/results/<search_request>')
+def results(search_request):
+    error_message = None
+    results = []
+    
+    try:
+        search_url = f'http://www.omdbapi.com/?apikey={OMDB_API_KEY}&s={search_request}'
+        search_response = requests.get(search_url)
+        search_response.raise_for_status()
+        search_data = search_response.json()
+
+        if search_data.get('Response') == 'True':
+            results = search_data.get('Search', [])
             detailed_results = []
 
             for result in results:
                 details_url = f'http://www.omdbapi.com/?apikey={OMDB_API_KEY}&i={result["imdbID"]}'
-                details_response = requests.get(details_url).json()
-                detailed_results.append(details_response)
+                details_response = requests.get(details_url)
+                details_response.raise_for_status()
+                detailed_results.append(details_response.json())
 
-            return render_template('select.html', results=detailed_results)
+            return render_template('select.html', results=detailed_results, search_request=search_request)
         else:
-            error = search_response.get('Error', 'No results found.')
-            return render_template('index.html', error=error)
-    return render_template('index.html')
+            error_message = search_data.get('Error', 'No results found.')
 
+    except requests.exceptions.RequestException as e:
+        error_message = str(e)
+
+    return render_template('index.html', error=error_message)
+
+# Watch endpoint with caching for details
+@cache.cached(timeout=300, query_string=True)
 @app.route('/watch/<title>')
 def watch(title):
+    title = title.replace('-', ' ')
     details_url = f'http://www.omdbapi.com/?apikey={OMDB_API_KEY}&t={title}'
-    details_response = requests.get(details_url).json()
+    
+    try:
+        details_response = requests.get(details_url)
+        details_response.raise_for_status()
+        details_data = details_response.json()
+    except requests.exceptions.RequestException as e:
+        return render_template('watch.html', error_message=str(e))
 
-    imdb_id = details_response.get('imdbID')
-    type    = details_response.get('Type', "N/A")
-    title   = details_response.get('Title', "N/A")
-    plot    = details_response.get('Plot', "N/A")
-    plot    = '<br>'.join(plot[i:i + 223] for i in range(0, len(plot), 225))
+    imdb_id = details_data.get('imdbID')
+    type = details_data.get('Type', "N/A")
+    title = details_data.get('Title', "N/A")
+    plot = details_data.get('Plot', "N/A")
+    plot = '<br>'.join(plot[i:i + 223] for i in range(0, len(plot), 225))
 
+    season = int(request.args.get('season', 1))
+    episode = int(request.args.get('episode', 1))
+
+    # Gather season/episode data for series
     if type == 'series':
-        embed_url = f'https://vidsrc.xyz/embed/tv/{imdb_id}?ads=false'
+        embed_url = f'https://vidsrc.xyz/embed/tv/{imdb_id}/{season}-{episode}?ads=false'
+        total_seasons = int(details_data.get('totalSeasons', 0))
+        seasons_object = {}
+        for s in range(1, total_seasons + 1):
+            season_url = f'http://www.omdbapi.com/?apikey={OMDB_API_KEY}&i={imdb_id}&Season={s}'
+            season_response = requests.get(season_url)
+            season_response.raise_for_status()  # Raises an error for bad responses
+            episodes = season_response.json().get('Episodes', [])
+            seasons_object[f'Season {s}'] = [
+                {"title": ep["Title"], "episode": ep["Episode"]} for ep in episodes
+            ]
     else:
         embed_url = f'https://vidsrc.xyz/embed/movie/{imdb_id}?ads=false'
-    
-    return render_template('watch.html', embed_url=embed_url, title=title, plot=plot)
+        seasons_object = None
+        total_seasons = None
+
+    return render_template('watch.html', embed_url=embed_url, title=title, plot=plot, type=type, seasons_object=seasons_object, current_episode=episode, current_season=season, total_seasons=total_seasons)
+
+# 404 error
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
